a6bf3d1e1abf76062d85e4601307c3f42754b4a6
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 14:14:29 +0600

    JavaAssignment-08-Inheritance-PRACTICE-Problem

--- src/LabeledPointE9_17.java
+++ src/LabeledPointE9_17.java
@@ -1,16 +1,13 @@
 import java.awt.Point;
 
 public class LabeledPointE9_17 {
-    private Point location;  // Using Point to store x and y coordinates
-    private String label;    // The label associated with the point
+    private Point location; 
+    private String label;  
 
-    // Constructor to initialize the location and label
     public LabeledPointE9_17(int x, int y, String label) {
-        this.location = new Point(x, y);  // Creating a new Point object
+        this.location = new Point(x, y); 
         this.label = label;
     }
-
-    // toString method to display the x, y coordinates and the label
     @Override
     public String toString() {
         return "LabeledPoint[" + location.toString() + ", label=" + label + "]";
