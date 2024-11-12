fbf84daccd06b3a4722e6227d8f7150fdd2422c5
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 13:51:32 +0600

    JavaAssignment 09 : Inheritance practice problem

--- src/MainE9_18.java
+++ src/MainE9_18.java
@@ -2,16 +2,12 @@ public class MainE9_18 {
     public static void main(String[] args) {
         CheckingAccountE9_18 account = new CheckingAccountE9_18(100.0); // Initial balance of $100
 
-        // Perform some transactions
-        account.deposit(50);  // Deposit $50
-        account.deposit(20);  // Deposit $20
-        account.deposit(10);  // Deposit $10
-        account.deposit(30);  // This deposit should incur a fee (4th transaction)
+        account.deposit(50);  
+        account.deposit(10);  
+        account.deposit(30);  
 
-        account.withdraw(40);  // Withdrawal of $40
-        account.withdraw(30);  // Withdrawal of $30 (5th transaction, should incur a fee)
-
-        // Print final balance and transaction count
+        account.withdraw(40);  
+        account.withdraw(30);  
         System.out.println("Final balance: " + account.getBalance());
         System.out.println("Total transactions: " + account.getTransactionCount());
     }
