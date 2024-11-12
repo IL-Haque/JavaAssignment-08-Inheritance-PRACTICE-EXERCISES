2ac257b88518754d0c11635634e521194abb7786
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 14:04:13 +0600

    JavaAssignment-08-Inheritance-PRACTICE-Problem

--- src/MainE9_17.java
+++ src/MainE9_17.java
@@ -1,10 +1,8 @@
 public class MainE9_17
 {
     public static void main(String[] args) {
-        // Create a LabeledPoint object
         LabeledPoint point = new LabeledPoint(5, 10, "Point A");
 
-        // Print the point using the toString method
         System.out.println(point);
     }
 }
