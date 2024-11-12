7d0b5ea4594ecc1a6c4d6ef44b2ff2649a6accb9
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 14:13:37 +0600

    JavaAssignment-08-Inheritance-PRACTICE-Problem

--- src/LabeledPoint.java
+++ src/LabeledPoint.java
@@ -3,14 +3,13 @@ public class LabeledPoint {
     private int y;
     private String label;
 
-    // Constructor to initialize the coordinates and label
+  
     public LabeledPoint(int x, int y, String label) {
         this.x = x;
         this.y = y;
         this.label = label;
     }
 
-    // toString method to display the x, y coordinates and the label
     @Override
     public String toString() {
         return "LabeledPoint[x=" + x + ", y=" + y + ", label=" + label + "]";
