1fd81dab64047ec687611084f5c170a1cd4a1867
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 14:06:08 +0600

    JavaAssignment-08-Inheritance-PRACTICE-Problem

--- src/MainE9__6.java
+++ src/MainE9__6.java
@@ -2,10 +2,10 @@ public class MainE9__6 {
     public static void main(String[] args) {
         FillInQuestion q = new FillInQuestion("The inventor of Java was _James Gosling_");
 
-        System.out.println(q.getText());  // Should display: "The inventor of Java was _____"
+        System.out.println(q.getText());  
 
-        System.out.println("User response: James Gosling - " + q.checkAnswer("James Gosling"));  // true
-        System.out.println("User response: james gosling - " + q.checkAnswer("james gosling"));  // true
-        System.out.println("User response: Gosling - " + q.checkAnswer("Gosling"));  // false
+        System.out.println("User response: James Gosling - " + q.checkAnswer("James Gosling")); 
+        System.out.println("User response: james gosling - " + q.checkAnswer("james gosling")); 
+        System.out.println("User response: Gosling - " + q.checkAnswer("Gosling"));  
     }
 }
