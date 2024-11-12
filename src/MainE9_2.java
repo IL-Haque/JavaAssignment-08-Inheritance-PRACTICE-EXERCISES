470c514b8a43335ccba32667292768d098733bde
Inzamamul Haque <162371402+IL-Haque@users.noreply.github.com>
2024-11-12 14:05:25 +0600

    JavaAssignment-08-Inheritance-PRACTICE-Problem

--- src/MainE9_2.java
+++ src/MainE9_2.java
@@ -1,18 +1,15 @@
-// Main.java
+
 public class MainE9_2 {
     public static void main(String[] args) {
         BasicAccount account = new BasicAccount(100.0);
 
-        // Display initial balance
         System.out.println("Initial balance: " + account.getBalance());
 
-        // Try to withdraw an amount within the balance
         account.withdraw(50.0);
 
-        // Attempt to withdraw more than the balance
         account.withdraw(100.0);
 
-        // Display final balance
+
         System.out.println("Final balance: " + account.getBalance());
     }
 }
